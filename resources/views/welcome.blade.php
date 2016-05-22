c68db97e526e91e314c2b6b389dcb524bdfd2876
hugotorres <hugotorreslinares@gmail.com>
2016-05-22 10:15:22 -0500

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">PETdelivery</div>
+                
             </div>
         </div>
     </body>
