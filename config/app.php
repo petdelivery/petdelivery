dd8249d094a156a584350b17d01cafc742860722
hugotorres <ingenierohugotorres@gmail.com>
2016-07-05 18:19:47 -0500

    sin intervention

--- config/app.php
+++ config/app.php
@@ -149,7 +149,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
 
         /*
@@ -205,7 +204,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' =>Collective\Html\HtmlFacade::class,
         'Input' => Illuminate\Support\Facades\Input::class,
