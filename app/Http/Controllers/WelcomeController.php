a3cd561b8c3139aefbb9ad1c22d8a0945e9aa655
hugotorres <ingenierohugotorres@gmail.com>
2016-07-11 17:22:57 -0500

    --ammend

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -29,26 +29,12 @@ class WelcomeController extends Controller
     {
 
        
-        $imagen1 = 'http://petdelivery.co/assets/images/home.jpg';
-        $imagen2 = 'http://petdelivery.co/assets/images/home2.jpg';
-
-        // Read image path, convert to base64 encoding
-        $image1Data = base64_encode(file_get_contents($imagen1));
-        $image2Data = base64_encode(file_get_contents($imagen2));
-
-        // Format the image SRC:  data:{mime};base64,{data};
-        $src1 = 'data: '.mime_content_type($imagen1).';base64,'.$image1Data;
-        $src2 = 'data: '.mime_content_type($imagen2).';base64,'.$image2Data;
-
-        $images = [$src1,$src2];
-        
-
 
         $posts=Post::where('destacado',1)->get();
         
 
 
-        return view('welcome',['articles'=>$posts,'images'=>$images]);
+        return view('welcome',['articles'=>$posts]);
     }
 
     public function productDetail($id)
