bacdcbbbcbd728ffebe944b46aeea0d642b23fe1
hugotorres <ingenierohugotorres@gmail.com>
2016-07-11 17:01:15 -0500

    --ammend

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -29,16 +29,16 @@ class WelcomeController extends Controller
     {
 
        
-        $images[0] = 'http://petdelivery.co/assets/images/home.jpg';
-        $images[1] = 'http://petdelivery.co/assets/images/home2.jpg';
+        $imagen1 = 'http://petdelivery.co/assets/images/home.jpg';
+        $imagen2 = 'http://petdelivery.co/assets/images/home2.jpg';
 
         // Read image path, convert to base64 encoding
-        $image1Data = base64_encode(file_get_contents($images[0]));
-        $image2Data = base64_encode(file_get_contents($images[1]));
+        $image1Data = base64_encode(file_get_contents($imagen1));
+        $image2Data = base64_encode(file_get_contents($imagen2));
 
         // Format the image SRC:  data:{mime};base64,{data};
-        $src1 = 'data: '.mime_content_type($images[0]).';base64,'.$image1Data;
-        $src2 = 'data: '.mime_content_type($images[1]).';base64,'.$image2Data;
+        $src1 = 'data: '.mime_content_type($imagen1).';base64,'.$image1Data;
+        $src2 = 'data: '.mime_content_type($imagen2).';base64,'.$image2Data;
 
         $images = [$src1,$src2];
         
