dff5049ed377e3319f2582a8c20b0b54104f6dde
hugotorres <ingenierohugotorres@gmail.com>
2016-07-22 09:26:33 -0500

    meta description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="description" content="pet shop bogota zona norte, entregaa domicilio, las mejores marcas de comida para gato o perro al mejor precio.">
     <title>PETDelivery</title>
 
     <!-- Fonts -->
