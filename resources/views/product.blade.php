f13b3740c9c9cd7b3694b6bdc41d9a4191ccbf7d
hugotorres <ingenierohugotorres@gmail.com>
2016-07-22 17:20:07 -0500

    asda

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,8 +6,8 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading text-center"> 
-                   <h1>La mejor comida para mascotas al norte de Bogotà</h1> 
-					<img src="/assets/images/logo.png" alt="">
+                   <h1><img src="/assets/images/logo.png" alt="petdelivery " width="70">La mejor comida para mascotas al norte de Bogotà</h1> 
+					
                 </div>
 
                 <div class="panel-body">
